8e8e3213105ad742adbae2be87c8e1b53ca60cf6
patterncat <patterncat@gmail.com>
2017-01-07 12:08:32 +0800

    init

---
+++ src/main/java/com/example/BenchDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenchDemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/controller/ConfigController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import com.example.config.EmbeddedTomcatConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * Created by patterncat on 2017-01-04.
+ */
+@RestController
+@RequestMapping("/config")
+public class ConfigController {
+
+    @Autowired
+    EmbeddedTomcatConfig embeddedTomcatConfig;
+
+    @RequestMapping("/tomcat")
+    public Map<String,Object> getServerConfigs(){
+        return embeddedTomcatConfig.getAttributes();
+    }
+}
